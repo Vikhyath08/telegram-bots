a15dc125d24f7e18d743c755a99f93bf03a5dcee
Vikhyath08 <vikhyath.pv@gmail.com>
2020-10-25 13:15:38 +0530

    Chain messages 4

--- api/hex-bot.ts
+++ api/hex-bot.ts
@@ -95,7 +95,8 @@ const talkInfoWizard = new WizardScene('talk_info',
     for (let key in talk){
       talkInfo += key + ':' + talk[key] + '\n';
     }
-    ctx.reply(talkInfo + '\n\nAwesome. That should be it for now. Thanks for using the bot!')
+    ctx.reply(talkInfo);
+    ctx.telegram.sendMessage(ctx.message.chat.id, 'Awesome. That should be it for now. Thanks for using the bot!');
     return ctx.scene.leave();
   },
 );
