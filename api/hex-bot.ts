e71cec6fc41e412939ea784e59711d72926a730a
Vikhyath08 <vikhyath.pv@gmail.com>
2020-10-25 14:01:50 +0530

    All types of commands

--- api/hex-bot.ts
+++ api/hex-bot.ts
@@ -105,6 +105,7 @@ const stage = new Stage([talkInfoWizard])
 bot.use(stage.middleware());
 const regex = /^\/([^@\s]+)@?(?:(\S+)|)\s?([\s\S]+)?$/i;
 
+
 bot.use(Telegraf.log());
 bot.use(async (ctx: ContextMessageUpdate, next) => {
   const start = new Date();
@@ -125,55 +126,55 @@ bot.command('AddIssue', Stage.enter('talk_info'));
 bot.command('leave', Stage.leave('talk_info'));
 
 // bot.command('AddIssue',async (ctx: ContextMessageUpdate) =>{
-//   // ctx.reply('Done')
-//   // return;
-//   if(ctx.from!.first_name){
-//     var rawData = fs.readFileSync('members.json');
+  //   // ctx.reply('Done')
+  //   // return;
+  //   if(ctx.from!.first_name){
+    //     var rawData = fs.readFileSync('members.json');
 //     var memberData = JSON.parse(rawData);
 //     const git_id = memberData[(ctx.from!.first_name)];
 //     // questioning further for description
 //     if(git_id){
-//       const content = {
-//         'Title':'Dummy',
-//         'Body':'Details',
-//         'Difficulty':0,
-//         'author':git_id,
-//         };
-//       const cops_bot_link = process.env.HEXIE_BOT_LINK || '';
-//       const response = await axios.post(cops_bot_link,content,{
-//             headers:{
-//               authorization:process.env.PASSWORD
+  //       const content = {
+    //         'Title':'Dummy',
+    //         'Body':'Details',
+    //         'Difficulty':0,
+    //         'author':git_id,
+    //         };
+    //       const cops_bot_link = process.env.HEXIE_BOT_LINK || '';
+    //       const response = await axios.post(cops_bot_link,content,{
+      //             headers:{
+        //               authorization:process.env.PASSWORD
 //             }
 //           });
 //       if (response.status === 200){
-//         ctx.reply('Thank you for Posting'); // Add in a link of the issue created
-//       } else {
-//         ctx.reply('There is some problem while creating your DevTalk');
-//       }
+  //         ctx.reply('Thank you for Posting'); // Add in a link of the issue created
+  //       } else {
+    //         ctx.reply('There is some problem while creating your DevTalk');
+    //       }
+    
+    //     } else {
+      //       ctx.reply('Register Yourself by /AddMe <your-github_id>');
+      //     }
+      //   }
+      //   ctx.reply('Hello fellow nerd, you have no name!!');
+      // });
       
-//     } else {
-//       ctx.reply('Register Yourself by /AddMe <your-github_id>');
-//     }
-//   }
-//   ctx.reply('Hello fellow nerd, you have no name!!');
-// });
-
-bot.command('CloseIssue',async (ctx: ContextMessageUpdate) => {
-  // Complete it the same way as above
-  ctx.reply('Done');
-});
-
-bot.command('UpdateIssue',async (ctx: ContextMessageUpdate)=>{
-  // Complete it the same way as above
-  ctx.reply('Done');
-});
-
-bot.command('AddMe',async (ctx: ContextMessageUpdate) => {
-  if(ctx.message && ctx.message.text){
-    const parts = regex.exec(ctx.message.text.trim());
-    if (parts) {
-      const command = {
-        text: ctx.message.text,
+      bot.command('CloseIssue',async (ctx: ContextMessageUpdate) => {
+        // Complete it the same way as above
+        ctx.reply('Done');
+      });
+      
+      bot.command('UpdateIssue',async (ctx: ContextMessageUpdate)=>{
+        // Complete it the same way as above
+        ctx.reply('Done');
+      });
+      
+      bot.command('AddMe',async (ctx: ContextMessageUpdate) => {
+        if(ctx.message && ctx.message.text){
+          const parts = regex.exec(ctx.message.text.trim());
+          if (parts) {
+            const command = {
+              text: ctx.message.text,
         command: parts[1],
         bot: parts[2],
         args: parts[3],
@@ -182,13 +183,13 @@ bot.command('AddMe',async (ctx: ContextMessageUpdate) => {
       var memberData = JSON.parse(rawData);
       memberData[ctx.from!.first_name]=command.args;
       fs.writeFile('members.json',
-                    JSON.stringify(memberData),
-                    (err) => {
-                      if(err){
-                        console.log("Error not saved");
-                      }
-                    }
-                  );
+      JSON.stringify(memberData),
+      (err) => {
+        if(err){
+          console.log("Error not saved");
+        }
+      }
+      );
       // In place of text fetch github profile and present
       await bot.telegram.sendMessage(ctx.from!.id,'text',{reply_to_message_id: ctx.message.message_id});
     }
@@ -203,23 +204,25 @@ bot.command('DevTalks', async (ctx: ContextMessageUpdate) => {
     owner: 'COPS-IITBHU',
     repo: 'DevTalks',
   });
-
+  
   if (data.length == 0) {
     ctx.reply('No upcoming dev talks.');
   }
-
+  
   const msgList = data.map(
     (element) => `[${element.title}](${element.html_url}) by [${element.user.login}](${element.user.html_url})`,
-  );
-
-  ctx.replyWithMarkdown(msgList.join('\n\n'), <ExtraEditMessage>Extra.webPreview(false));
-});
-
-if (!PROD_ENV) {
-  bot.launch();
-}
-
-module.exports = (req: NowRequest, resp: NowResponse) => {
-  if (req.method === 'POST') bot.handleUpdate(req.body, resp);
+    );
+    
+    ctx.replyWithMarkdown(msgList.join('\n\n'), <ExtraEditMessage>Extra.webPreview(false));
+  });
+  
+  bot.on('text', ctx => ctx.reply('This command is not supported 😅.'));
+  
+  if (!PROD_ENV) {
+    bot.launch();
+  }
+  
+  module.exports = (req: NowRequest, resp: NowResponse) => {
+    if (req.method === 'POST') bot.handleUpdate(req.body, resp);
   else resp.status(200).send('Use POST to use Telegram bot!');
 };
